ee54d4f36edb1a8ef9c5bc0c8860dc1c57fe0c9c
wpecommerce <wpecommerce@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-20 02:49:01 +0000

    tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-isell-photo/tags/1.0.0@614807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,99 @@
+=== WP iSell Photo ===
+Contributors: wpecommerce
+Donate link: http://wp-ecommerce.net/
+Tags: sell photos wordpress, sell photos, photos, wordpress photo selling, sell photo wordpress, wordpress gallery, photo gallery, photography, sell images, sell digital print
+Requires at least: 3.0
+Tested up to: 3.4.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily Sell photos, images, digital print etc. using the built-in WordPress gallery feature. Convert your WordPress gallery into a photo store.
+
+== Description ==
+
+WP iSell Photo enhances the functionality of your existing WordPress photo gallery and turns it into an e-commerce photo gallery. It makes  photo selling easier. You don't have to maintain another heavy weight photo gallery plugin for your WordPress blog. This in turn should help you maintain a fast loading site.
+
+= WP iSell Photo Features =
+
+* Sell photos from your WordPress blog easily.
+* Increase your photo selling conversion rate with one-click PayPal checkout.
+* Create beautiful e-commerce photo gallery on your WordPress blog.
+* No advanced technical knowledge required to use this photo selling plugin.
+
+= WP iSell Photo Plugin Usage =
+
+Since WordPress 2.5 there is a new feature in the WordPress media library that allows you to create a gallery of photos/images and add it to a post/page. Lot of users don't even know about this neat little feature of WordPress. WP iSell Photo plugin will help you convert a built in WordPress gallery into a photo selling platform.
+
+**a)** Creating a Photo Gallery in WordPress
+
+Create a new post/page on your WordPress Dashboard. There is an option to upload/insert media. Now select the photos/images from your computer and upload them to the media library. As you upload each image you will see a "Gallery" tab which contains those images. Switch to that tab once you are ready to insert the gallery to your current post/page.
+
+There are some options that you can configure for the gallery you just created (under the "Gallery Settings" section).
+
+1. Link thumbnails to: a) Image file  b) Attachment Page - you can either link each thumbnail to its URL or the attachment page (the page/post you are currently editing)
+1. Order images by: a) Menu order  b) Title  c) Date/TIme  d) Random
+1. Order: a) Ascending   b) Descending
+1. Gallery columns: 1 - 9 ( Number of thumbnails in each row)
+
+Finally hit the "Insert Gallery" button and the gallery will be automatically inserted to your current post/page.
+
+If you want to edit the gallery at any time you can always select the gallery (It looks like a rectangular image in the visual editor) and click on the "Edit" option. Alternatively you can also customize the shortcode for the gallery. You need to switch to the "HTML" editor to do it. You will see a shortcode like the following:
+
+`[gallery]`
+
+For more information on how to customize the WordPress gallery shortcode please refer to the [WordPress Documentation](http://codex.wordpress.org/Gallery_Shortcode)
+
+**b)** WP iSell Photo Settings
+
+There are some options that you need to configure in the General Settings of the plugin before your site goes live. On your *WordPress Dashboard* under *Settings* click on the *WP iSell Photo* option. It will take you to the Settings page.
+
+* PayPal Email Address: Your PayPal email address
+* PayPal Currency: The currency code (e.g. USD, GBP etc)
+* Currency Symbol: The symbol for your currency code (e.g. $). It's for display purpose only.
+* Return URL: The URL where your customer will be redirected to after a successful payment
+
+**c)** Creating Buy Now buttons to sell photos
+
+Go to the post/page where you already have an existing gallery embedded. Add an additional `amount` parameter to the gallery shortcode and specify the price in it. For example:
+
+`[gallery amount="5.00"]`
+
+now each image of the current gallery will have a "Buy Now" button with price 5.00
+
+You can also customize the look and feel of the "Buy Now" button. Simply include a `button` parameter in the gallery shortcode and specify the text you want to use for the button. For example:
+
+`[gallery amount="3.99" button="Buy it Now"]`
+
+If you want to use an image for the button you can do so by specifying the URL in the `button` parameter. For example:
+
+`[gallery amount="3.99" button="http://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif"]`
+
+For detailed documentation please visit the [WordPress iSell Photo](http://wp-ecommerce.net/wp-isell-photo-easily-sell-photos-wordpress-1800) plugin page
+
+== Installation ==
+
+1. Go to the Add New plugins screen in your WordPress admin area
+1. Click the upload tab
+1. Browse for the plugin file (wp-iSell-photo.zip)
+1. Click Install Now and then activate the plugin
+1. Now, embed a gallery on a post/page to sell photos from your WordPress blog.
+
+== Frequently Asked Questions ==
+
+= Can this plugin be used to sell photos in WordPress =
+
+Yes.
+
+= Can this plugin be used to sell WordPress media library images? =
+
+Yes.
+
+== Screenshots ==
+
+For screenshots please visit the [WordPress Sell Photo](http://wp-ecommerce.net/wp-isell-photo-easily-sell-photos-wordpress-1800) plugin page
+
+== Changelog ==
+
+= 1.0.0 =
+* First commit
---
+++ wp-iSell-photo.php
@@ -0,0 +1,312 @@
+<?php
+/*
+Plugin Name: WP iSell Photo
+Version: 1.0.0
+Plugin URI: http://wp-ecommerce.net/?p=1800
+Author: wpecommerce
+Author URI: http://wp-ecommerce.net/
+Description: A simple plugin to sell photos from WordPress
+*/
+
+if(!defined('ABSPATH')) exit;
+if(!class_exists('WP_iSELL_PHOTO'))
+{
+	class WP_iSELL_PHOTO 
+	{
+		var $plugin_version = '1.0.0';
+		function __construct() 
+		{
+			define('WP_iSELL_PHOTO_VERSION', $this->plugin_version);
+			$this->plugin_includes();
+		}
+		function plugin_includes()
+		{
+			if(is_admin( ) ) 
+			{
+				add_filter('plugin_action_links', array(&$this,'wp_iSell_photo_plugin_action_links'), 10, 2 );
+			}
+			add_action('admin_menu', array( &$this, 'wp_iSell_photo_add_options_menu' ));
+			add_filter('post_gallery', 'wp_iSell_photo_gallery_shortcode', 10, 2 );	
+		}
+		function plugin_url() 
+		{
+			if($this->plugin_url) return $this->plugin_url;
+			return $this->plugin_url = plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+		}
+		function wp_iSell_photo_plugin_action_links($links, $file) 
+		{
+			if ( $file == plugin_basename( dirname( __FILE__ ) . '/wp-iSell-photo.php' ) ) 
+			{
+				$links[] = '<a href="options-general.php?page=wp-iSell-photo-settings">Settings</a>';
+			}
+			return $links;
+		}
+		
+		function wp_iSell_photo_add_options_menu()
+		{
+			if(is_admin())
+			{
+				add_options_page('WP iSell Photo Settings', 'WP iSell Photo', 'manage_options', 'wp-iSell-photo-settings', array(&$this, 'wp_iSell_photo_options_page'));
+			}
+			add_action('admin_init', array(&$this, 'wp_iSell_photo_add_settings'));		
+		}
+		function wp_iSell_photo_add_settings()
+		{ 
+			register_setting('wp-iSell-photo-settings-group', 'wp_iSell_photo_paypal_email_address');
+			register_setting('wp-iSell-photo-settings-group', 'wp_iSell_photo_paypal_currency_code');
+			register_setting('wp-iSell-photo-settings-group', 'wp_iSell_photo_paypal_currency_symbol');
+			register_setting('wp-iSell-photo-settings-group', 'wp_iSell_photo_paypal_return_url');	
+		}
+		function wp_iSell_photo_options_page()
+		{
+			
+			?>
+			<div class="wrap">
+			<div id="poststuff"><div id="post-body">
+			
+			<h2>WP iSell Photo - v<?php echo $this->plugin_version;?></h2>
+			<div class="postbox">
+			<h3><label for="title">General Settings</label></h3>
+			<div class="inside">		
+			<form method="post" action="options.php">
+			    <?php settings_fields('wp-iSell-photo-settings-group'); ?>
+			    <table class="form-table">
+			        <tr valign="top">
+			        <th scope="row">PayPal Email Address</th>
+			        <td><input type="text" name="wp_iSell_photo_paypal_email_address" size="60" value="<?php echo get_option('wp_iSell_photo_paypal_email_address'); ?>" />
+			        <p><i>Your PayPal email address</i></p>
+			        </td>
+			        </tr>
+			         
+			        <tr valign="top">
+			        <th scope="row">PayPal Currency</th>
+			        <td><input type="text" name="wp_iSell_photo_paypal_currency_code" size="40" value="<?php echo get_option('wp_iSell_photo_paypal_currency_code'); ?>" />
+			        <p><i>Your PayPal Currency (Example: USD, EUR, GBP, AUD etc)</i></p>
+			        </td>
+			        </tr>
+			        
+			        <tr valign="top">
+			        <th scope="row">Currency Symbol</th>
+			        <td><input type="text" name="wp_iSell_photo_paypal_currency_symbol" size="20" value="<?php echo get_option('wp_iSell_photo_paypal_currency_symbol'); ?>" />
+			        <p><i>(Example: $)</i></p>
+			        </td>
+			        </tr>
+			        
+			        <tr valign="top">
+			        <th scope="row">Return URL</th>
+			        <td><input type="text" name="wp_iSell_photo_paypal_return_url" size="80" value="<?php echo get_option('wp_iSell_photo_paypal_return_url'); ?>" />
+			        <p><i>The url where your customer will be redirected to after a successful payment</i></p>
+			        </td>
+			        </tr>
+			    </table>
+			    
+			    <p class="submit">
+			    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			    </p>		
+			</form>
+			</div></div>
+			
+			</div></div>
+			</div>
+			<?php
+		}
+	}
+	$GLOBALS['wp_iSell_photo'] = new WP_iSELL_PHOTO();
+}
+
+function wp_iSell_photo_gallery_shortcode($output,$attr) 
+{
+	
+	/* Custom gallery shortcode. */
+	global $post;
+
+	static $instance = 0;
+	$instance++;
+
+	// Allow plugins/themes to override the default gallery template.
+	/*
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+	*/
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => '',
+		'amount'	 => '',  // plugin specific parameter
+		'button'	 => ''   // plugin specific parameter
+	), $attr));
+	
+	/* plugin specific check */
+	$error_msg = "";
+	$paypal_email = get_option('wp_iSell_photo_paypal_email_address');
+	$currency = get_option('wp_iSell_photo_paypal_currency_code');
+	$return_url = get_option('wp_iSell_photo_paypal_return_url');
+	$currency_symbol = get_option('wp_iSell_photo_paypal_currency_symbol');
+	if(empty($paypal_email)) 
+	{
+		$error_msg .= '<div style="color:red;">You did not specify a PayPal email address in the settings</div>';	
+	}
+	if(empty($currency)) 
+	{
+		$error_msg .= '<div style="color:red;">You did not specify a currency code in the settings</div>';	
+	}
+	if(empty($currency_symbol)) 
+	{
+		$error_msg .= '<div style="color:red;">You did not specify a currency symbol in the settings</div>';	
+	}
+	if(empty($return_url)) 
+	{
+		$error_msg .= '<div style="color:red;">You did not specify a return url in the settings</div>';
+	}
+	if(empty($amount))
+	{
+		$error_msg .= '<div style="color:red;">You did not specify a price in the shortcode</div>';	
+	}
+	if(empty($button))
+	{
+		$button = "Buy Now";		
+	}
+	if(!empty($error_msg))
+	{
+		return $error_msg;
+	}
+	/* end check */
+	
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+		</style>
+		<!-- see gallery_shortcode() in wp-includes/media.php -->";
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+		$url = wp_get_attachment_url($attachment->ID);
+		$item_name = $attachment->post_title;
+		$amount = number_format($amount, 2, '.', '');
+		//$item_description = '<div class="wpiSellPhoto_item_description">Item: '.$item_name.'</div>';
+		$item_price = '<div class="wpiSellPhoto_item_price">Price: '.$currency_symbol.$amount.'</div>';
+		$button_code = wp_iSell_photo_get_button_code_for_paypal($paypal_email,$currency,$return_url,$item_name,$amount,$button);
+		$output .= "<{$itemtag} class='gallery-item'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+				$item_price
+				$button_code
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />';
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+		</div>\n";
+
+	return $output;
+
+}
+
+function wp_iSell_photo_get_button_code_for_paypal($paypal_email,$currency,$return_url,$item_name,$amount,$button)
+{
+	$image_button = strstr($button, 'http');
+	if($image_button==FALSE)
+	{
+		$button = '<input type="submit" class="wpiSellPhoto_buy_now_button" value="'.$button.'">';	
+	}
+	else
+	{
+		$button = '<input type="image" src="'.$button.'" border="0" name="submit" alt="'.$item_name.'">';
+	}
+	$button_code = <<<EOT
+	<form method="post" action="https://www.paypal.com/cgi-bin/webscr">
+	<input type="hidden" name="cmd" value="_xclick">
+	<input type="hidden" name="business" value="$paypal_email">
+	<input type="hidden" name="item_name" value="$item_name">
+	<input type="hidden" name="amount" value="$amount">
+	<input type="hidden" name="currency_code" value="$currency">
+	<input type="hidden" name="return" value="$return_url">
+	$button
+	</form>
+EOT;
+	return $button_code;
+}
+?>
